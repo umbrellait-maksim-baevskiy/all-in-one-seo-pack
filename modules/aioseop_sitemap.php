af5ba3cb55808e7e0245369edb71ee92cd561445
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-06-23 19:49:23 -0400

    #70 update static sitemap when posts are published, updated, or deleted

--- modules/aioseop_sitemap.php
+++ modules/aioseop_sitemap.php
@@ -320,6 +320,34 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Sitemap' ) ) {
 			add_filter( $this->prefix . 'output_option', array( $this, 'display_custom_options' ), 10, 2 );
 			add_action( $this->prefix . 'daily_update_cron', array( $this, 'daily_update' ) );
 			add_action( 'init', array( $this, 'make_dynamic_xsl') );
+			add_action( 'transition_post_status', array( $this, 'update_sitemap_from_posts'), 10, 3);
+		}
+
+		function update_sitemap_from_posts( $new_status, $old_status, $post ) {
+
+			// TODO if dynamic, delete transient (we currently don't do transients)
+
+
+			if ( $this->option_isset( 'rewrite' ) ) {
+				// TODO if dynamic, delete transient (we currently don't do transients)
+				return;
+			}
+
+			$posttypes = array();
+			if ( ! empty( $this->options["{$this->prefix}posttypes"] ) ) {
+				$posttypes = $this->options["{$this->prefix}posttypes"];
+			}
+
+			if ( ! in_array( $post->post_type, $posttypes, true ) ) {
+				return;
+			}
+
+			$statuses_for_updating = array( 'new', 'publish', 'trash' );
+			if ( ! in_array( $new_status, $statuses_for_updating, true ) ) {
+				return;
+			}
+
+			$this->do_sitemaps();
 		}
 
 		/**
