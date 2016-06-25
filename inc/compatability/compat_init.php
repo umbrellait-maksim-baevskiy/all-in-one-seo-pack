872d1cd57e2b0ebb60dc89f64ac2710587519bc0
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-06-24 20:39:56 -0400

    add compatability class

---
+++ inc/compatability/compat_init.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
+
+	/**
+	 * Class All_in_One_SEO_Pack_Compatibility
+	 */
+	class All_in_One_SEO_Pack_Compatibility {
+
+		function __construct() {
+
+			$this->load_compatibility_classes();
+
+		}
+
+		function load_compatibility_classes() {
+
+		}
+	}
+
+}
