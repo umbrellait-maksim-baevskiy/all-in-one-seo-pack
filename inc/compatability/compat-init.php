d659e8c3c017d9df1d24af95ac9a0f31580a2ba0
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-06-28 16:59:49 -0400

    WPCS for compat-init class

--- inc/compatability/compat-init.php
+++ inc/compatability/compat-init.php
@@ -23,7 +23,7 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
 		 *
 		 * @since 2.3.6
 		 */
-		function __construct() {
+		public function __construct() {
 
 			$this->load_compatibility_classes();
 
@@ -34,10 +34,10 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
 		 *
 		 * @since 2.3.6
 		 */
-		function load_compatibility_hooks() {
+		public function load_compatibility_hooks() {
 			// We'll use this until we set up out classes.
-
 			if ( class_exists( 'jetpack' ) ) {
+
 				add_filter( 'jetpack_get_available_modules', array( $this, 'remove_jetpack_sitemap' ) );
 			}
 		}
@@ -50,7 +50,7 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
 		 * @since 2.3.6
 		 * @return mixed
 		 */
-		function remove_jetpack_sitemap( $modules ) {
+		public function remove_jetpack_sitemap( $modules ) {
 			// Remove Jetpack's sitemap
 			unset( $modules['sitemaps'] );
 
@@ -63,7 +63,7 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
 		 *
 		 * @since 2.3.6
 		 */
-		function load_compatibility_classes() {
+		public function load_compatibility_classes() {
 			// Eventually we'll load our other classes from here.
 			$this->load_compatibility_hooks();
 		}
@@ -72,4 +72,3 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Compatibility' ) ) {
 }
 
 $aiosp_compat = new All_in_One_SEO_Pack_Compatibility();
-
