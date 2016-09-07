22781f8a738e7ad930bbe20987c792fc5e1111c8
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-09-07 19:22:01 -0400

    changed wording for translator credit

--- admin/display/credits-content.php
+++ admin/display/credits-content.php
@@ -55,7 +55,7 @@
 	</li>
 </ul>
 
-<h3 class="wp-people-group"><?php printf( esc_html__( 'Translators for All in One SEO Pack %s', 'all-in-one-seo-pack' ), '1.2' ); ?></h3>
+<h3 class="wp-people-group"><?php printf( esc_html__( 'Translators Contributors and Translation Editors', 'all-in-one-seo-pack' ), '1.2' ); ?></h3>
 <p class="wp-credits-list">
 	<a href="https://profiles.wordpress.org/pierrelannoy/" target="_blank">Pierre Lannoy</a>,
 	<a href="https://profiles.wordpress.org/sonjanyc/" target="_blank">Sonja Leix</a>,
