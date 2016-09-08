e362c158d303afacc67d3c5c093990f4a6d83feb
wpsmort <smortiboy@semperfiwebdesign.com>
2016-09-08 12:07:37 -0400

    #451 Fixed an issue with the link to General Settings

--- admin/display/welcome-content.php
+++ admin/display/welcome-content.php
@@ -35,5 +35,5 @@
 			</div>
 		</div>
 	</div>
-	<p><a href="/wp-admin/admin.php?page=all-in-one-seo-pack/aioseop_class.php">Continue to the General Settings</a> &raquo;</p>
+	<p><a href="/wp-admin/admin.php?page=all-in-one-seo-pack%2Faioseop_class.php">Continue to the General Settings</a> &raquo;</p>
 </div>
