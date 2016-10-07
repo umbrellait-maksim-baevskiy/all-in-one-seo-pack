00529b00c7f3244932350c939e4db651ebe385ef
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-10-07 10:26:14 -0400

    remove build sitemap button #542

--- modules/aioseop_sitemap.php
+++ modules/aioseop_sitemap.php
@@ -630,10 +630,9 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Sitemap' ) ) {
 				$options[ $this->prefix . 'max_posts' ] = 50000;
 			}
 			$url                               = trailingslashit( get_home_url() ) . $options[ $this->prefix . 'filename' ] . '.xml';
-			$options[ $this->prefix . 'link' ] = sprintf( __( 'Please review your settings below and click %s to build your sitemap; then, %s.',
-				'all-in-one-seo-pack' ), sprintf( '<a href="#" onclick="document.dofollow.elements[\'Submit\'][0].click();">%s</a>',
-				__( 'Update Sitemap', 'all-in-one-seo-pack' ) ), '<a href="' . esc_url( $url ) . '" target="_blank">' .
-			                                                     __( 'view your sitemap', 'all-in-one-seo-pack' ) . '</a>' );
+
+			$options[ $this->prefix . 'link' ] = sprintf( __( 'Click here to %s.', 'all-in-one-seo-pack' ), '<a href="' . esc_url( $url ) . '" target="_blank">' . __( 'view your sitemap', 'all-in-one-seo-pack' ) . '</a>' );
+
 			if ( $this->option_isset( 'rewrite' ) ) {
 
 				$options[ $this->prefix . 'link' ] .= '<p>' . __( 'Note: you are using dynamic sitemap generation to keep your sitemap current; this will not generate a static sitemap file.', 'all-in-one-seo-pack' ) . '</p>';
