7e0d5d71c8fe2ed1af0a5fdec6e471cc242f676f
Michael Torbert <michaeltorbert@users.noreply.github.com>
2016-10-26 17:20:06 -0400

    fix broken link on welcome page #295

--- admin/display/welcome-content.php
+++ admin/display/welcome-content.php
@@ -33,5 +33,5 @@
 			</div>
 		</div>
 	</div>
-	<p><a href="/wp-admin/admin.php?page=all-in-one-seo-pack%2Faioseop_class.php"><?php _e( 'Continue to the General Settings', 'all-in-one-seo-pack' ); ?></a> &raquo;</p>
+	<p><a href=" <?php echo get_admin_url( null, "admin.php?page=" . AIOSEOP_PLUGIN_DIRNAME . "/aioseop_class.php" ); ?>  "><?php _e( 'Continue to the General Settings', 'all-in-one-seo-pack' ); ?></a> &raquo;</p>
 </div>
