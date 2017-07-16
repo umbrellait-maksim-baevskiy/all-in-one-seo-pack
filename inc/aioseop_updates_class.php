ddfb4d81f1c52a28331371aabd23dbba222d153b
Michael Torbert <michaeltorbert@users.noreply.github.com>
2017-07-16 17:26:01 -0400

    really remove welcome screen on update

--- inc/aioseop_updates_class.php
+++ inc/aioseop_updates_class.php
@@ -57,7 +57,8 @@ class AIOSEOP_Updates {
 			}
 
 			if( ! is_network_admin() || !isset( $_GET['activate-multi'] ) ) {
-				set_transient( '_aioseop_activation_redirect', true, 30 ); // Sets 30 second transient for welcome screen redirect on activation.
+				// Replace this to reactivate update welcome screen.
+				//set_transient( '_aioseop_activation_redirect', true, 30 ); // Sets 30 second transient for welcome screen redirect on activation.
 			}
 			delete_transient( 'aioseop_feed' );
 			add_action( 'admin_init', array( $this, 'aioseop_welcome' ) );
