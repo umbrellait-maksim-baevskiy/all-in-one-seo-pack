7537dabe71c610579ef0375a068917e3f71d907e
contactashish13 <contactashish13@users.noreply.github.com>
2017-09-27 20:29:53 +0530

    XML Sitemap - Include Video XML Sitemap URL in sitemap index #201 (#1145)
    XML Sitemap - Include Video XML Sitemap URL in sitemap index

--- modules/aioseop_sitemap.php
+++ modules/aioseop_sitemap.php
@@ -1650,8 +1650,8 @@ if ( ! class_exists( 'All_in_One_SEO_Pack_Sitemap' ) ) {
 				);
 			}
 
-			$files	= apply_filters( 'aioseop_sitemap_index_filenames', $files, $prefix, $suffix );
-			
+			$files  = apply_filters( 'aioseop_sitemap_index_filenames', $files, $prefix, $suffix );
+
 			return $files;
 		}
 
