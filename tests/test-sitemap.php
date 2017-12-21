e43014f0d0ece0476f3a327433e0d27c5dc0fb96
contactashish13 <contactashish13@gmail.com>
2017-12-21 12:07:58 +0530

    framework + sitemap unit tests

--- tests/test-sitemap.php
+++ tests/test-sitemap.php
@@ -8,102 +8,104 @@
 /**
  * Sitemap test case.
  */
-class Test_Sitemap extends WP_Ajax_UnitTestCase {
 
-	private function _has_in_sitemap( $link, $column, $value ) {
-		$this->go_to(site_url( '/sitemap.xml'));
-		$output = ob_get_contents();
-		ob_clean();
-		print_r($output);
+require_once dirname( __FILE__ ) . '/aioseop-test-base.php';
+
+class Test_Sitemap extends AIOSEOP_Unit_Test_Base {
+
+	public function setUp(){
+		parent::init();
+		parent::setUp();
 	}
 
-	private function _setup_options() {
-        $this->_setRole( 'administrator' );
-		// init the general options.
-		do_action( 'init' );
-		global $aioseop_options;
-
-		// activate the sitemap module.
-		$aioseop_options['modules'] = array(
-			'aiosp_feature_manager_options'	=> array(
-				'aiosp_feature_manager_enable_sitemap'	=> 'on'
-			),
-		);
-		update_option( 'aioseop_options', $aioseop_options );
-
-		$_POST		= array(
-			'action'				=> 'aiosp_update_module',
-			'Submit_All_Default'	=> 'blah',
-			'Submit'				=> 'blah',
-			'nonce-aioseop'			=> wp_create_nonce( 'aioseop-nonce' ),
-			'module'				=> 'All_in_One_SEO_Pack',
-		);
-
-		$this->go_to( admin_url( 'admin.php?page=all-in-one-seo-pack%2Fmodules%2Faioseop_sitemap.php' ) );
-		do_action( 'admin_menu' );
-
-		$aioseop_options = get_option( 'aioseop_options' );
-//print_r($aioseop_options);
-return;
-
-		unset( $aioseop_options['aiosp_sitemap_indexes'] );
-		unset( $aioseop_options['aiosp_sitemap_images'] );
-		unset( $aioseop_options['aiosp_sitemap_gzipped'] );
-		$aioseop_options['aiosp_posttypecolumns'] = array( 'post' );
-		update_option( 'aioseop_options', $aioseop_options );
+	public function tearDown(){
+		parent::init();
+		parent::tearDown();
 	}
 
-	private function _upload_image( $image, $id = 0 ) {
-		/* this factory method has a bug so we have to be a little clever.
-		$this->factory->attachment->create( array( 'file' => $image, 'post_parent' => $id ) );
-		*/
-		$attachment_id = $this->factory->attachment->create_upload_object( $image, $id );
-		if ( 0 !== $id ) {
-			update_post_meta( $id, '_thumbnail_id', $attachment_id );
-		}
+	public function test_only_pages() {
+		$posts = $this->setup_posts( 2 );
+		$pages = $this->setup_posts( 2, 0, 'page' );
+
+		$custom_options = array();
+		$custom_options['aiosp_sitemap_indexes'] = '';
+		$custom_options['aiosp_sitemap_images'] = '';
+		$custom_options['aiosp_sitemap_gzipped'] = '';
+		$custom_options['aiosp_sitemap_posttypes'] = array( 'page' );
+
+		$this->_setup_options( 'sitemap', $custom_options );
+
+		$this->validate_sitemap(
+			array(
+					$pages['without'][0] => true,
+					$pages['without'][1] => true,
+					$posts['without'][0] => false,
+					$posts['without'][1] => false,					
+			)
+		);
 	}
 
 	public function test_featured_image() {
-		$this->factory->post->create_many( 2, array( 'post_content' => 'content without image', 'post_title' => 'title without image' ) );
-		$ids	= $this->factory->post->create_many( 2, array( 'post_content' => 'content with image', 'post_title' => 'title with image' ) );
-		foreach ( $ids as $id ) {
-			$this->_upload_image( str_replace( '\\', '/', trailingslashit( __DIR__ ) . 'images/footer-logo.png' ), $id );
-		}
+		$posts = $this->setup_posts( 2, 2 );
 
-		$posts	= get_posts(
+		$custom_options = array();
+		$custom_options['aiosp_sitemap_indexes'] = '';
+		$custom_options['aiosp_sitemap_images'] = '';
+		$custom_options['aiosp_sitemap_gzipped'] = '';
+		$custom_options['aiosp_sitemap_posttypes'] = array( 'post' );
+
+		$this->_setup_options( 'sitemap', $custom_options );
+
+		$with = $posts['with'];
+		$without = $posts['without'];
+		$this->validate_sitemap(
 			array(
-				'post_type' => 'post',
-				'fields'	=> 'ids',
-		) );
-
-		// 4 posts created?
-		$this->assertEquals(4, count( $posts ) );
-
-		$attachments	= get_posts(
-			array(
-				'post_type' => 'attachment',
-				'fields'	=> 'ids',
-		) );
-
-		// 2 attachments created?
-		$this->assertEquals(2, count( $attachments ) );
-
-		$featured	= 0;
-		foreach ( $posts as $id ) {
-			if ( has_post_thumbnail( $id ) ) {
-				$featured++;
-			}
-		}
-
-		// 2 posts have featured image?
-		$this->assertEquals(2, $featured);
-
-		$this->_setup_options();
-
-		$this->_has_in_sitemap( '', '', '' );
+					$with[0] => array(
+						'image'	=> true,
+					),
+					$with[1] => array(
+						'image'	=> true,
+					),
+					$without[0] => array(
+						'image'	=> false,
+					),
+					$without[1] => array(
+						'image'	=> false,
+					),
+			)
+		);
 	}
 
+	public function test_exclude_images() {
+		$posts = $this->setup_posts( 2, 2 );
 
+		$custom_options = array();
+		$custom_options['aiosp_sitemap_indexes'] = '';
+		$custom_options['aiosp_sitemap_images'] = 'on';
+		$custom_options['aiosp_sitemap_gzipped'] = '';
+		$custom_options['aiosp_sitemap_posttypes'] = array( 'post' );
+
+		$this->_setup_options( 'sitemap', $custom_options );
+
+		$with = $posts['with'];
+		$without = $posts['without'];
+		$this->validate_sitemap(
+			array(
+					$with[0] => array(
+						'image'	=> false,
+					),
+					$with[1] => array(
+						'image'	=> false,
+					),
+					$without[0] => array(
+						'image'	=> false,
+					),
+					$without[1] => array(
+						'image'	=> false,
+					),
+			)
+		);
+	}
 }
 
 
