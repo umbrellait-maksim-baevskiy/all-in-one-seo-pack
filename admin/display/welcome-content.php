c5c8e4d802903084f1958e964724997a9a97d807
Michael Torbert <michaeltorbert@users.noreply.github.com>
2019-05-31 19:05:46 -0400

    Welcome page changes for 3.0 (#2539)
    * fix variable name
    
    * welcome page updates
    
    * Use tabs instead of spaces to indent
    
    * Fix Grunt issuees in welcome-content.php
    
    * Remove whitespace at end of line

--- admin/display/welcome-content.php
+++ admin/display/welcome-content.php
@@ -1,10 +1,16 @@
 <div class="welcome-panel">
 	<div class="welcome-panel-content">
 		<div class="welcome-panel-column-container">
+			<div>
+				<h3><a href="https://semperplugins.com/all-in-one-seo-pack-3-0/" target="_blank"><?php echo esc_html( sprintf( __( "Check out what's new in our %s release post!", 'all_in_one_seo_pack' ), 'Version 3' ) ); ?></a></h3>
+			</div>
 			<div class="welcome-panel-column">
-				<h3><?php
+				<h3>
+					<?php
 					/* translators: %s is a placeholder, which means that it should not be translated. It will be replaced with the name of the plugin, All in One SEO Pack. */
-					echo esc_html( sprintf( __( 'Support %s', 'all-in-one-seo-pack' ), AIOSEOP_PLUGIN_NAME ) ); ?></h3>
+					echo esc_html( sprintf( __( 'Support %s', 'all-in-one-seo-pack' ), AIOSEOP_PLUGIN_NAME ) );
+					?>
+				</h3>
 				<p class="message welcome-icon welcome-edit-page">
 				<?php
 					/* translators: %s is a placeholder, which means that it should not be translated. It will be replaced with the name of the plugin, All in One SEO Pack. */
