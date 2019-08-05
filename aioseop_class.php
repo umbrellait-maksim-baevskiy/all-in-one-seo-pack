d76974ed7119fc73e529572a857b797f1e00669d
Arnaud Broes <abroes@outlook.be>
2019-08-06 00:41:32 +0200

    Don't output default robots meta tag value (#2777)
    * Don't output index,follow #2768
    
    * Delete launch.json #2768
    
    * Fix Grunt issue #2768

--- aioseop_class.php
+++ aioseop_class.php
@@ -4201,6 +4201,7 @@ class All_in_One_SEO_Pack extends All_in_One_SEO_Pack_Module {
 		}
 
 		if ( ! $this->is_page_included() ) {
+
 			/**
 			 * The aioseop_robots_meta filter hook.
 			 *
@@ -4213,9 +4214,9 @@ class All_in_One_SEO_Pack extends All_in_One_SEO_Pack_Module {
 			 * @return string
 			 */
 			$robots_meta = apply_filters( 'aioseop_robots_meta', $this->get_robots_meta() );
-			if ( ! empty( $robots_meta ) ) {
-				// Should plugin & version details be added here as well?
-				echo '<meta name="robots" content="' . esc_attr( $robots_meta ) . '" />' . "\n";
+
+			if ( ! empty( $robots_meta ) && 'index,follow' !== $robots_meta ) {
+				printf( '<meta name="robots" content="%s"', esc_attr( $robots_meta ) ) . " >\n";
 			}
 
 			if ( ! empty( $old_wp_query ) ) {
@@ -4320,7 +4321,7 @@ class All_in_One_SEO_Pack extends All_in_One_SEO_Pack_Module {
 				$meta_string .= sprintf( "<meta name=\"keywords\" %s content=\"%s\" />\n", $key_attr, $keywords );
 			}
 		}
-		// Handle noindex, nofollow - robots meta.
+
 		if ( get_option( 'blog_public' ) ) {
 
 			/**
@@ -4336,7 +4337,7 @@ class All_in_One_SEO_Pack extends All_in_One_SEO_Pack_Module {
 			 */
 			$robots_meta = apply_filters( 'aioseop_robots_meta', $this->get_robots_meta() );
 
-			if ( ! empty( $robots_meta ) ) {
+			if ( ! empty( $robots_meta ) && 'index,follow' !== $robots_meta ) {
 				$meta_string .= sprintf( '<meta name="robots" content="%s"', esc_attr( $robots_meta ) ) . " />\n";
 			}
 		}
