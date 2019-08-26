3edb3a9354bb321fddf69da4724fbbcb5d9ad7b1
EkoJR <ekojr1337@gmail.com>
2019-08-26 13:29:42 -0700

    Improve Travis yml Configuration (#2820)
    * Change Travis CI test builds & PHPUnit version
    
    * Change travis.yml syntax
    
    * Add documentation to travis.yml
    
    * Add cache to travis.yml
    
    * Add commit depth to travis.yml
    
    * Add dist setting to travis.yml
    
    * Change install-phpunit.sh to be encapsulated
    
    * Remove PHP 5.2 & 5.3 matrix builds unsupported by PHPUnit

--- bin/install-phpunit.sh
+++ bin/install-phpunit.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
-PHPUNIT_VERSION=$1;
-PHP_VERSION=$2;
+# Setup PHPUnit in Travis CI environment.
 
-echo 'PHPUnit Version: '$PHPUNIT_VERSION;
-echo 'PHP Version: '$PHP_VERSION;
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <PHPUnit Version> <PHP Version>"
+	exit 1
+fi
 
-if [[ 'local' = $PHPUNIT_VERSION ]];
-then
-  composer install;
-else
-  composer config platform.php $PHP_VERSION;
-  composer require --dev phpunit/phpunit '^'$PHPUNIT_VERSION;
-fi;
+setup_phpunit() {
+  local PHPUNIT_VERSION=$1;
+  local PHP_VERSION=$2;
+
+  echo 'PHPUnit Version: '$PHPUNIT_VERSION;
+  echo 'PHP Version: '$PHP_VERSION;
+
+  if [[ 'local' = $PHPUNIT_VERSION ]];
+  then
+    composer install;
+  else
+    composer config platform.php $PHP_VERSION;
+    composer require --dev phpunit/phpunit '^'$PHPUNIT_VERSION;
+  fi;
+}
+
+setup_phpunit $1 $2;
